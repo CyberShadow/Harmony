ce5a82ac9353297b4757cbf99fe93e58eaeb6285
Vladimir Panteleev <git@thecybershadow.net>
2020-02-05 00:21:13 +0000

    Harmony/Documentation/examples/test.sh: Test in parallel

--- Harmony/Documentation/examples/test.sh
+++ Harmony/Documentation/examples/test.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 set -eu
 
-for f in *.cs
-do
-	csc /reference:../../obj/Debug/net35/0Harmony.dll /target:library /out:/dev/null "$f"
-done
+find . -name '*.cs' -print0 | xargs -P 0 -0 -n 1 csc /nologo /reference:../../obj/Debug/net35/0Harmony.dll /target:library /out:/dev/null
