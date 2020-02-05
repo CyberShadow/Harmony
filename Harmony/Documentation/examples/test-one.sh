b906e0e6d4ac955b61abf377785f7b2f0053cb85
Vladimir Panteleev <git@thecybershadow.net>
2020-02-05 01:57:31 +0000

    Harmony/Documentation/examples/test-one.sh: Don't hardcode dll path

--- Harmony/Documentation/examples/test-one.sh
+++ Harmony/Documentation/examples/test-one.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec csc /nologo /reference:../../obj/Debug/net35/0Harmony.dll /target:library /out:/dev/null "$1" $(sed -n 's/.*extra-arg: \(.*\)$/\1/p' "$1")
+exec csc /nologo /reference:"$(find ../../obj -name 0Harmony.dll | head -n 1)" /target:library /out:/dev/null "$1" $(sed -n 's/.*extra-arg: \(.*\)$/\1/p' "$1")
